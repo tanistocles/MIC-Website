f0cc27d8b05d9ac0beb69b84b7a8a846dee8e8a5
gitdev1 <gitdev1@gmail.com>
2025-04-16 10:58:56 +1200

    split dashboard

--- mic-website/src/app/admin/MainPage.jsx
+++ mic-website/src/app/admin/MainPage.jsx
@@ -46,46 +46,6 @@ function MainPage() {
             </div>
         </div>
         </div>
-        <div class="widgets-grid">
-            <div class="widget">
-                <h2>Quick Stats</h2>
-                <p>Posts: 12</p>
-                <p>Users: 5</p>
-                <p>Comments: 23</p>
-            </div>
-
-            <div class="widget">
-                <h2>Recent Posts</h2>
-                <table class="post-table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Sample Post 1</td>
-                            <td>Admin</td>
-                            <td>2024-03-20</td>
-                        </tr>
-                        <tr>
-                            <td>Sample Post 2</td>
-                            <td>Editor</td>
-                            <td>2024-03-19</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <div class="widget">
-                <h2>System Info</h2>
-                <p>Version: 1.0.0</p>
-                <p>Theme: Default</p>
-                <p>Storage: 64MB / 1GB</p>
-            </div>
-        </div>
     </main>
         </>
     )
--- mic-website/src/app/admin/page.tsx
+++ mic-website/src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 import './Style.css';
 import Navbar from './Navbar';
-// import MainPage from './MainPage';
+import MainPage from './MainPage';
 import AllPost from './AllPost';
 import Category from './Category';
 import NewPost from './NewPost';
@@ -8,17 +8,19 @@ import Library from './Library';
 import FormList from './FormList';
 import AllPages from './AllPages';
 import AllUsers from './AllUsers';
+import BodyMain from './BodyMain';
 
 function Admin() {
   return (
     <>
       <Navbar />
-      {/* <MainPage /> */}
+      <MainPage />
+      <BodyMain />
       {/* <AllPost /> */}
       {/* <Category /> */}
       {/* <NewPost /> */}
       {/* <Library /> */}
-      <AllPages />
+      {/* <AllPages /> */}
       {/* <FormList /> */}
       {/* <AllUsers /> */}
     </>
