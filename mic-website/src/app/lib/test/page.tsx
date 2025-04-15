205441569ce9ea51367680f294e1cffb5cfd48db
aflahaamrin <aflaha.amrin@gmail.com>
2025-04-15 18:03:07 +1200

    Add sqlite3 database

---
+++ mic-website/src/app/lib/test/page.tsx
@@ -0,0 +1,15 @@
+import { Header, Footer, NavBar, Slider } from '../../ui';
+import Details from "./Details"
+
+export default function Contact() {
+    return(
+
+        <div>
+            <Header/>
+            <Slider/>
+            <NavBar/>
+            <Details/>
+            <Footer/>
+        </div>
+    )
+}
